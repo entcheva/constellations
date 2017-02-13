52191c91942d85f69020612919092fd2a9027e25
entcheva <christina.entcheva@gmail.com>
2017-02-13 13:30:14 -0500

    Fix UserController typo

---
+++ constellationsAPI/app/api/v1/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+module Api::V1
+  class UsersController < ApplicationController
+    def index
+    end
+
+    def show
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def delete
+    end
+
+    private
+
+    def user_params
+    end
+  end
+end
