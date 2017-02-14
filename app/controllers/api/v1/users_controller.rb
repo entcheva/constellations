890dbf80efb916d0266b90770ac4e2417be5abb7
github username <github email address>
2017-02-14 14:31:56 -0500

    complete sign up auth

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,19 +1,45 @@
-class Api::V1::UsersController < ApplicationController
+module Api::V1
+  class UsersController < ApplicationController
 
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      jwt = Auth.encrypt({user_id: @user.id})
-      render json: {jwt: jwt, email: @user.email}
+    def show
     end
+
+    def create
+      user = User.new(user_params)
+      if params[:password] == params[:password_confirmation]
+        user.password = params[:password]
+        if user.save
+          jwt = Auth.encrypt({user: user.id}) # uses our encrypt method to return an encrypted user token
+          render json: {jwt: jwt} # sends token to react action
+        else
+          render json: {:errors=>
+           [{:detail=>"incorrect email or password",
+             :source=>{:pointer=>"user/err_type"}}
+           ]}, status: 404
+        end
+      else
+        render json: {:errors=>
+         [{:detail=>"failed password verification",
+           :source=>{:pointer=>"user/err_type"}}
+         ]}, status: 404
+      end
+    end
+
+
+    def edit
+    end
+
+    def update
+    end
+
+    def delete
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    end
+
   end
-
-
-
-  private
-
-  def user_params
-    params.permit(:email, :password, :password_confirmation)
-  end
-
 end
